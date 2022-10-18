a5b055beb64ae76030bace745387f2adbe00e264
CodePit84 <104715795+CodePit84@users.noreply.github.com>
2022-10-18 11:34:30 +0200

    Exo 3
    Créez une classe représentant une personne. Elle doit avoir les propriétés nom, prénom et adresse, ainsi qu’un constructeur et un destructeur. Une méthode getpersonne() doit retourner les coordonnées complètes de la personne. Une méthode setadresse() doit permettre de modifier l’adresse de la personne. Créez des objets personne, et utilisez l’ensemble des méthodes.

---
+++ Personne.php
@@ -0,0 +1,50 @@
+<?php
+class Personne {
+
+    public string $nom;
+    public string $prenom;
+    public string $adresse;
+
+    public function __construct(string $nom, string $prenom, string $adresse)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->adresse = $adresse;
+    }
+
+    public function getpersonne()
+    {
+        return $this->nom . " " . $this->prenom . "\n" . $this->adresse . "\n" . "\n";
+    }
+
+    public function setadresse($newAdresse)
+    {
+        $this->adresse = $newAdresse;
+    }
+
+    public function __destruct()
+    {
+        exit;
+        // Je ne sais pas quoi mettre à part exit pour être certain de détruire l'objet
+    }
+}
+
+$personne1 = new Personne("Doe", "John", "45 rue des fleurs 84200 Carpentras");
+$personne2 = new Personne('Dupont', 'Marie', '67 rue du lac 75018 Paris');
+$personne3 = new Personne('Mbappé', 'Kylian', '167 rue du Parc 75016 Paris');
+
+// var_dump($personne1);
+
+echo $personne1->getpersonne();
+echo $personne2->getpersonne();
+echo $personne3->getpersonne();
+echo "\n";
+echo "Nouvelle adresse de " . $personne2->prenom . " :";
+$personne2->setadresse('2 rue nouvelle 84000 Avignon');
+echo "\n";
+echo "vérif changement de : \n";
+echo $personne2->getpersonne();
+echo "On détuit la 3ème personne et on l'affiche : \n";
+$personne3->__destruct();
+echo $personne3->getpersonne();
+
